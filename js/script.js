bd2472df3278fac9ddc716cc56c99cd96fafb57d
MattiaToscano <mattia1997ct@gmail.com>
2025-03-27 14:10:09 +0100

    [UPD] Implementata visualizzazione form a fine conto alla rovescia

--- js/script.js
+++ js/script.js
@@ -2,9 +2,23 @@
 const countdown = document.getElementById('countdown'); 
 console.log(countdown);
 
+// Riprendo lista dei numeri randomici
+const numeriRandom = document.getElementById('numbers-list');
+
+//Riprendo le istruzioni
+const istructions = document.getElementById('instructions');
+
+//Riprendo bottone
+const bottone = document.querySelector('button');
+
+//Riprendo form
+const form = document.getElementById('answers-form');
+
+//Numeri randomici fino a 50
+const random = Math.floor(Math.random() * 50) + 1;
 
 // Aggiorna il countdown ogni secondo
-let tempoRimasto = 15; 
+let tempoRimasto = 4; 
 
 const timer = setInterval(() => {
     if (tempoRimasto > 0) {
@@ -12,6 +26,17 @@ const timer = setInterval(() => {
         tempoRimasto--;
     } else {
         clearInterval(timer);
+        istructions.innerText = 'Inserisci i numeri che hai visto in precedenza'; // Mostra le istruzioni
+        form.classList.remove('d-none'); // Mostra il form
         countdown.innerHTML = 'Tempo scaduto'; // Mostra il messaggio di fine countdown
     }
 }, 1000);
+
+//Riprendo i numeri da generare e inserire randomicamente
+
+function
+
+
+
+
+
